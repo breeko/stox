0953c83efb3844f15f11cdead026285181d74ccf
Branko Blagojevic <branko.blagojevic@gmail.com>
2019-12-09 06:39:06 -0500

    remove secret

--- src/api.ts
+++ src/api.ts
@@ -1,13 +1,13 @@
 import rp from "request-promise"
 import {
   StockHistoricalResponse, StockHistory, StockMetric, StockMetricsResponse, StockProfile, StockProfileResponse } from "./Response"
-import { Secret } from "./secret"
+// import { Secret } from "./secret"
 import { convertDate, convertNumeric, parseDate } from "./utils"
 
 const Urls = {
     history: (stock: string, from: Date, to: Date) => `https://financialmodelingprep.com/api/v3/historical-price-full/${stock}?from=${convertDate(from)}&to=${convertDate(to)}`,
     metrics: (stock: string) => `https://financialmodelingprep.com/api/v3/company-key-metrics/${stock}?period=quarter`,
-    news: (stock: string, before: Date) => `https://newsapi.org/v2/everything?q=${stock}&from=${before}&apiKey=${Secret.NEWS_API_KEY}`,
+    // news: (stock: string, before: Date) => `https://newsapi.org/v2/everything?q=${stock}&from=${before}&apiKey=${Secret.NEWS_API_KEY}`,
     newsRaw: (stock: string, before: Date) => `https://news.google.com/search?q=${stock}+before:${convertDate(before)}&hl=en-US&gl=US&ceid=US:en`,
     profile: (stock: string) => `https://financialmodelingprep.com/api/v3/company/profile/${stock}`
 };
