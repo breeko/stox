68b3a26a41150a492acda04f4da6821f774aae2a
Branko Blagojevic <branko.blagojevic@gmail.com>
2019-12-09 06:41:25 -0500

    remove secret again

--- src/Urls.ts
+++ src/Urls.ts
@@ -1,10 +1,10 @@
-import {Secret} from "./secret"
+// import {Secret} from "./secret"
 import {convertDate} from "./utils"
 
 export const URLS = {
     history: (stock: string, from: Date, to: Date) => `https://financialmodelingprep.com/api/v3/historical-price-full/${stock}?from=${convertDate(from)}&to=${convertDate(to)}`,
     metrics: (stock: string) => `https://financialmodelingprep.com/api/v3/company-key-metrics/${stock}?period=quarter`,
-    news: (stock: string, before: Date) => `https://newsapi.org/v2/everything?q=${stock}&from=${before}&apiKey=${Secret.NEWS_API_KEY}`,
+    // news: (stock: string, before: Date) => `https://newsapi.org/v2/everything?q=${stock}&from=${before}&apiKey=${Secret.NEWS_API_KEY}`,
     newsRaw: (stock: string, before: Date) => `https://news.google.com/search?q=${stock}+before:${convertDate(before)}&hl=en-US&gl=US&ceid=US:en`,
     profile: (stock: string) => `https://financialmodelingprep.com/api/v3/company/profile/${stock}`,
 }
